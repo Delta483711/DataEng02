4d8842feb7c2cb17f83791c3dd457e53b5b53a98
Michael <59081538+Delta483711@users.noreply.github.com>
2025-08-09 21:18:37 +0100

    Created int model and completed build updated stg model to match styling of int model

--- netflix_Proj/models/stage/stg_netflix.sql
+++ netflix_Proj/models/stage/stg_netflix.sql
@@ -1,16 +1,16 @@
 
-WITH UpdateCasting AS (
+WITH updatecasting AS (
     SELECT
-       cast("Show_Id" AS varchar) as id,
-       cast("Category" as varchar) as category,
-       cast("Title" as varchar) as title,   
-       cast("Director" as varchar) as director,
-       cast("Cast" as varchar) as cast, 
-       cast("Country" as varchar) as country,
-       cast("Release_Date" AS date) as releasedate,
-       cast("Rating" as varchar) as rating,
-       cast("Duration" as varchar) as duration
+       CAST("Show_Id" AS VARCHAR) AS id,
+       CAST("Category" AS VARCHAR) AS category,
+       CAST("Title" AS VARCHAR) AS title,   
+       CAST("Director" AS VARCHAR) AS director,
+       CAST("Cast" AS VARCHAR) AS cast, 
+       CAST("Country" AS VARCHAR) AS country,
+       CAST("Release_Date" AS date) AS releasedate,
+       CAST("Rating" AS VARCHAR) AS rating,
+       CAST("Duration" AS VARCHAR) AS duration
     FROM
         {{ source('raw', 'NetflixData') }}
 )
-SELECT * FROM UpdateCasting
+SELECT * FROM updatecasting
