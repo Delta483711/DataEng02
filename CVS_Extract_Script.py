4464460282c19a682b30536ff40bab788b41922f
Michael <59081538+Delta483711@users.noreply.github.com>
2025-08-09 20:06:27 +0100

    Extracted the data from csv file from github. Add the data to Neon DB

--- CVS_Extract_Script.py
+++ CVS_Extract_Script.py
@@ -1,5 +1,35 @@
 ## Import Librairies 
-from io import StringIO
+import pandas as pd
+from init import ConnectionToDatabase
+from sqlalchemy import create_engine
 
-link ="https://github.com/Delta483711/DataEng02/blob/main/Netflix_Dataset.csv"
-df = pd.read_csv(StringIO(requests.get(url).text))
+# Raw file link from GitHub
+url = "https://raw.githubusercontent.com/Delta483711/DataEng02/main/Netflix_Dataset.csv"
+
+# Load CSV into a DataFrame
+df = pd.read_csv(url)
+
+# Show the first few rows
+#print(df.head())
+
+
+##### Connect to Neon or any other database ######
+
+## Declare Connection variables 
+username = 'neondb_owner'
+password = 'npg_p5mJ3qEGDBvX'
+host = 'ep-cool-surf-abds13vc-pooler.eu-west-2.aws.neon.tech'
+port = '5432'
+database = 'netflix'
+sslmode = 'require'  # Neon requires SSL
+
+## run the connection function 
+ConnectionToDatabase(username,password,host,port,database,sslmode)
+
+## Save to database 
+connection_string = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}?sslmode={sslmode}"
+engine = create_engine(connection_string)
+
+df.to_sql("CovidCSVTest", engine, schema="raw", if_exists="replace", index=False)
+
+print(" Data written to Neon!")
