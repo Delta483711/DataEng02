9701e9480704b842a2b7804d81a10a00b9c87bec
Michael <59081538+Delta483711@users.noreply.github.com>
2025-08-10 20:04:49 +0100

    Added More Stuff
    Added two new fields to mart
    Added new cards
    Added two new charts

--- netflix_Proj/models/mart/netflix_v1.sql
+++ netflix_Proj/models/mart/netflix_v1.sql
@@ -11,10 +11,22 @@ SELECT
     t1.release_date,
     t1.rating,
     t1.duration_min,
-    t1.duration_season
+    t1.duration_season,
+    COUNT(CASE WHEN t1.category = 'Movie' THEN 1 ELSE NULL END) AS total_movie_count,
+    COUNT(CASE WHEN t1.category = 'TV Show' THEN 1 ELSE NULL END) AS total_tv_show_count
 
 FROM {{ ref('int_netflix') }} AS t1
-
+GROUP BY 
+    t1.id,
+    t1.category,
+    t1.title,
+    t1.director,
+    t1.cast,
+    t1.country,
+    t1.release_date,
+    t1.rating,
+    t1.duration_min,
+    t1.duration_season
 )
 
 SELECT * FROM Main
